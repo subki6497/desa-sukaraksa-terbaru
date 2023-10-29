fadc1b573e91a5c41e3a96a19fafadac08ff8b7a
subki6497 <149304629+subki6497@users.noreply.github.com>
2023-10-29 18:56:08 +0700

    Update nama kabupaten

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -10,7 +10,7 @@
       <!-- Sesuaikan nama desa masing-masing -->
       <p class="text-white text-3xl">Desa Sukaraksa</p>
       <p class="text-white text-3xl">Kecamatan Cigudeg</p>
-      <p class="text-white text-3xl">Kab. Bogor</p>
+      <p class="text-white text-3xl">Kabupaten Bogor</p>
     </div>
     <div class="grid place-items-center ml-auto mr-20">
       <iframe
