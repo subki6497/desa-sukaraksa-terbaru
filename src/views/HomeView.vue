6f7167b25e16d7198136e85e12d650804dc2f401
subki6497 <149304629+subki6497@users.noreply.github.com>
2023-10-30 21:20:22 +0700

    Update paragraf baru

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -37,7 +37,6 @@ import Sidebar from "../components/Sidebar.vue";
           karakter dasar dari masyarakat desa dan juga bangsa Indonesia.
         </li>
       </ul>
-       </p>
        <p class="text-lg font-semibold">Misi</p>
       <ul>
 
